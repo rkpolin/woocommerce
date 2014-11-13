23574e3220e09d4d89b3b1e85d0d543b35cca2af
polin <rkpolin@gmail.com>
2014-11-13 12:56:29 +0600

    v1.1

---
+++ wp-content/themes/Eshopper/functions.php
@@ -0,0 +1,38 @@
+<?php
+require_once('wp_bootstrap_navwalker.php');
+/*Latest Jquery*/
+function my_latest_jquery() {
+		wp_enqueue_script('jquery');
+}
+add_action('init', 'my_latest_jquery');
+
+	// add menu support and fallback menu if menu doesn't exist
+	register_nav_menus( array(
+    'primary' => __( 'Primary Menu', 'eshoppers' ),
+    'shop_menu' => __( 'Shop Menu', 'eshoppers' ),
+) );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
--- wp-content/themes/Eshopper/header.php
+++ wp-content/themes/Eshopper/header.php
@@ -87,13 +87,19 @@
 					</div>
 					<div class="col-sm-8">
 						<div class="shop-menu pull-right">
-							<ul class="nav navbar-nav">
-								<li><a href="#"><i class="fa fa-user"></i> Account</a></li>
-								<li><a href="#"><i class="fa fa-star"></i> Wishlist</a></li>
-								<li><a href="checkout.html"><i class="fa fa-crosshairs"></i> Checkout</a></li>
-								<li><a href="cart.html"><i class="fa fa-shopping-cart"></i> Cart</a></li>
-								<li><a href="login.html"><i class="fa fa-lock"></i> Login</a></li>
-							</ul>
+							<?php
+								wp_nav_menu( array(
+									'menu'              => 'shop_menu',
+									'theme_location'    => 'shop_menu',
+									'depth'             => 2,
+									'container'         => 'div',
+									'container_class'   => 'collapse navbar-collapse',
+									'container_id'      => 'bs-example-navbar-collapse-1',
+									'menu_class'        => 'nav navbar-nav',
+									'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+									'walker'            => new wp_bootstrap_navwalker())
+								);
+							?>
 						</div>
 					</div>
 				</div>
@@ -113,26 +119,19 @@
 							</button>
 						</div>
 						<div class="mainmenu pull-left">
-							<ul class="nav navbar-nav collapse navbar-collapse">
-								<li><a href="index.html" class="active">Home</a></li>
-								<li class="dropdown"><a href="#">Shop<i class="fa fa-angle-down"></i></a>
-                                    <ul role="menu" class="sub-menu">
-                                        <li><a href="shop.html">Products</a></li>
-										<li><a href="product-details.html">Product Details</a></li> 
-										<li><a href="checkout.html">Checkout</a></li> 
-										<li><a href="cart.html">Cart</a></li> 
-										<li><a href="login.html">Login</a></li> 
-                                    </ul>
-                                </li> 
-								<li class="dropdown"><a href="#">Blog<i class="fa fa-angle-down"></i></a>
-                                    <ul role="menu" class="sub-menu">
-                                        <li><a href="blog.html">Blog List</a></li>
-										<li><a href="blog-single.html">Blog Single</a></li>
-                                    </ul>
-                                </li> 
-								<li><a href="404.html">404</a></li>
-								<li><a href="contact-us.html">Contact</a></li>
-							</ul>
+							<?php
+								wp_nav_menu( array(
+									'menu'              => 'primary',
+									'theme_location'    => 'primary',
+									'depth'             => 2,
+									'container'         => 'div',
+									'container_class'   => 'collapse navbar-collapse',
+									'container_id'      => 'bs-example-navbar-collapse-1',
+									'menu_class'        => 'nav navbar-nav',
+									'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+									'walker'            => new wp_bootstrap_navwalker())
+								);
+							?>
 						</div>
 					</div>
 					<div class="col-sm-3">
